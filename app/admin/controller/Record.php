e95e781e0c5eb0265aad90ee27f011ff6a78ef2d
weige <916+948336@qq.com>
2019-08-09 17:25:56 +0800

    03

--- app/admin/controller/Record.php
+++ app/admin/controller/Record.php
@@ -9,16 +9,32 @@ class Record extends Controller{
         if($_POST){
             $list = $_POST;
             $id = $_POST['r_id'];
+            $data = Db::table('record')->where('r_id','=',$id)->find();
             $image = RecordModel::upload_img();
             unset($list['r_id']);
             if(!empty($image['wei'])){
                 $list['r_weima'] = $image['wei'];
+                if(!empty($data['r_weima'])){
+                    if(file_exists(ROOT_PATH . 'public' . DS . 'uploads'. DS . $data['r_weima'])){
+                        unlink(ROOT_PATH . 'public' . DS . 'uploads'. DS . $data['r_weima']);
+                    }
+                }
             }
             if(!empty($image['zhi'])){
                 $list['r_zhima'] = $image['zhi'];
+                if(!empty($data['r_zhima'])){
+                    if(file_exists(ROOT_PATH . 'public' . DS . 'uploads'. DS . $data['r_zhima'])){
+                        unlink(ROOT_PATH . 'public' . DS . 'uploads'. DS . $data['r_zhima']);
+                    }
+                }
             }
             if(!empty($image['logo'])){
                 $list['r_logo'] = $image['logo'];
+                if(!empty($data['r_logo'])){
+                    if(file_exists(ROOT_PATH . 'public' . DS . 'uploads'. DS . $data['r_logo'])){
+                        unlink(ROOT_PATH . 'public' . DS . 'uploads'. DS . $data['r_logo']);
+                    }
+                }
             }
             $record = new RecordModel();
             if($record->save($list,['r_id'=>$id])){
