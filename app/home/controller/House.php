fa841cae98b2981ebc08f100b99aa7a33b8941f3
weige <916+948336@qq.com>
2019-09-29 17:57:17 +0800

    a

--- app/home/controller/House.php
+++ app/home/controller/House.php
@@ -6,7 +6,7 @@ use think\Db;
 use think\Request;
 
 class House extends Controller{
-   public function hot(){
+   public function kuai_hot(){
        $house = new HouseModel();
        $map['h_level'] = 1;
        $map['h_tuijian'] = 2;
@@ -15,6 +15,24 @@ class House extends Controller{
        $list = json_encode($data,JSON_UNESCAPED_UNICODE);
        return $list;
    }
+
+    public function kuai_list(){
+        $house = new HouseModel();
+        $map['h_level'] = 1;
+        $map['h_status'] = 1;
+        $data = $house->where($map)->select();
+        $list = json_encode($data,JSON_UNESCAPED_UNICODE);
+        return $list;
+    }
+
+    public function kuai_detail(){
+        $house = new HouseModel();
+        $id = Request::instance()->param('id');
+        $data = $house->where('h_id','=',$id)->find();
+        $list = json_encode($data,JSON_UNESCAPED_UNICODE);
+        return $list;
+    }
+
 }
 
 
---
+++ app/home/controller/User.php
@@ -0,0 +1,26 @@
+<?php
+namespace  app\home\controller;
+use app\home\controller\Base;
+use think\Controller;
+use think\Db;
+use think\Request;
+
+class User extends Controller{
+   public function kuai_apply(){
+       $params = Request::instance()->param();
+       $id = is_int($params['id'])?$params['id']:intval($params['id']);
+       if($params['apply'] == 3){
+           $map = ['u_three'=>1];
+       }else if($params['apply'] == 4){
+           $map = ['u_four'=>1];
+       }
+       if(Db::table('user')->where('u_id','=',$id)->update($map)){
+           echo 1;
+       }else{
+           echo 0;
+       }
+   }
+}
+
+
+?>
