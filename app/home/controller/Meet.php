4e95566cd1398fc1c8ada86015c9d89a30a32dd6
weige <916948336@qq.com>
2019-10-03 19:19:28 +0800

    02

--- app/home/controller/Meet.php
+++ app/home/controller/Meet.php
@@ -19,18 +19,37 @@ class Meet extends controller{
 
     public function add(){
         $params = Request::instance()->param();
+        $hu_id = Db::table('house')->where('h_id','=',intval($params['h_id']))->find();
         $meet = new MeetModel();
         $map['mh_id'] = $params['h_id'];
         $map['mu_id'] = $params['u_id'];
         $map['m_time'] = $params['time'] ;
         $map['m_ctime'] = time();
         $map['m_content'] = $params['content'];
+        $map['mhu_id'] = $hu_id['h_id'];
         $list = $meet->save($map);
         if($list){
             echo 1;
         }else{
             echo 0;
         }
+    }
+
+    public function kuai_list_one(){
+        $params = Request::instance()->param();
+//        var_dump($params);
+//        var_dump($params['role']);die;
+        $test = $params['role'];
+        if($test == 1){
+            $keys = 'mu_id';
+        }else{
+            $keys = 'mhu_id';
+        }
+        $meet = new MeetModel();
+        $map['m_status'] = 1;
+        $map[$keys] = $params['id'];
+//        $list = $meet->where($map)->join('house h',)->select();
+//        var_dump($list);die;
 
     }
 
